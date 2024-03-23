154a1a8d34e24408ea7222f9b705a6cf18a15457
yanakiv30 <yanakiv30@gmail.com>
2024-03-23 20:36:41 +0200

    SearchOrder?

---
+++ src/features/order/SearchOrder.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function SearchOrder() {
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+
+function handleSubmit(e) {
+    e.prevent.default();
+    if(!query) return;
+    navigate(`/order/${query}`);
+    setQuery("");
+
+};
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        placeholder="Search order #"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+    </form>
+
+
+
+  );
+}
