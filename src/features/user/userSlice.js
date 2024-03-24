17e0e7d2d510728d385c7891d3f2251883333d3a
yanakiv30 <138647535+yanakiv30@users.noreply.github.com>
2024-03-24 21:06:02 +0200

    store-init

---
+++ src/features/user/userSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = { username: '' };
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    updateName(state, action) {
+      state.username = action.payload;
+    },
+  },
+});
+console.log(userSlice);
+export const {updateName}=userSlice.actions;
+export default userSlice.reducer;
