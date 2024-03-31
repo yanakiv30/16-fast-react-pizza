bd6fd9a38e3444aedfff4f5d96a8bb4632e5665f
yanakiv30 <138647535+yanakiv30@users.noreply.github.com>
2024-03-31 22:49:42 +0300

    cartSlice

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState={
+    // cart:[],
+ cart:[
+    {
+      pizzaId:12,
+      name: "Mediteranean",
+      quantity:2,
+      unitPrice:16,
+      totalPrice:32
+    },
+ ],
+
+}
+
+const cartSlice= createSlice({
+    name:"cart",
+    initialState,
+    reducers:{
+        addItem(state,action){},
+        deleteItem(state,action){},
+        increaseItemQuantity(state,action){},
+        decreaseItemQuantity(state,action){},
+        clearCart(state,action){},
+
+    }
+
+})
