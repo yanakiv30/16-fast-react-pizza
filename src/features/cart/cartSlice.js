5ed08af5804abe1730c6400f38d724d6ec04c289
yanakiv30 <yanakiv30@gmail.com>
2024-04-01 16:12:43 +0300

    cartSlice in progress

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -18,12 +18,32 @@ const cartSlice= createSlice({
     name:"cart",
     initialState,
     reducers:{
-        addItem(state,action){},
-        deleteItem(state,action){},
-        increaseItemQuantity(state,action){},
-        decreaseItemQuantity(state,action){},
-        clearCart(state,action){},
+        addItem(state,action){
+            //payload=newItem
+            state.cart.push(action.payload);
+        },
+        deleteItem(state,action){
+            //payload= pizzaId
+            state.cart= state.cart.filter(item=> item.pizzaId!==action.payload)
+        },
+        increaseItemQuantity(state,action){
+            //payload=pizzaId
+            const item= state.cart.find(item=> item.pizzaId===action.payload);
+            item.quantity ++;
+            item.totalPrice=item.quantity * item.unitPrice;
 
+        },
+        decreaseItemQuantity(state,action){
+            //payload=pizzaId
+            const item= state.cart.find(item=> item.pizzaId===action.payload);
+            item.quantity --;
+            item.totalPrice=item.quantity * item.unitPrice;
+        },
+        clearCart(state){
+            state.cart=[];
+        },
     }
-
-})
+});
+       export const{addItem, deleteItem, increaseItemQuantity, 
+        decreaseItemQuantity, clearCart}= cartSlice.actions;
+            export default cartSlice.reducer;
